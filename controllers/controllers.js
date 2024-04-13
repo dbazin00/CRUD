fef80ba87cc7ea910d4b62cb7f81a3fd23d5f34c
Davor Bazina <dbazina71@outlook.com>
2024-04-13 16:57:27 +0200

    Refacotr getAllTodos function

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -2,10 +2,10 @@ const Todo = require("../models/todo");
 
 const getAllTodos = async (req, res) => {
   const sortOptions = ["DESC", "ASC"];
-  const sort = sortOptions.includes(req.query.sort) ? req.query.sort : "DESC";
+  const { sort } = req.query;
 
   const allTodos = await Todo.findAll({
-    order: [["createdAt", sort]],
+    order: [["createdAt", sortOptions[Number(sort === "ASC")]]],
   });
 
   res.send({ res: allTodos });
